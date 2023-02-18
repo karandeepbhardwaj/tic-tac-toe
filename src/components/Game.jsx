32f8ea50a6c8a6389b02627a7925142d820bcbf1
Karandeep Bhardwaj <karandeepbhardwaj@karandeeps-air.home>
2023-02-18 15:43:55 -0500

    removing node modules from git

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,12 +5,16 @@ import { useState } from "react";
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  let currentSquare;
+  const currentSquare = history[history.length -1];
+
+  function handlePlay() {
+
+  }
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} currentSquare={currentSquare} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol></ol>
